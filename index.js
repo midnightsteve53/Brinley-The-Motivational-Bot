21fe5a60cecd0e50d2af318057d5007a8cc9c0f4
midnightsteve53 <74148290+midnightsteve53@users.noreply.github.com>
2020-11-08 12:03:09 -0500

    Add bot logic file

---
+++ index.js
@@ -0,0 +1,52 @@
+require('dotenv').config(); // Pulls in authorization information.
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if(!message.content.startsWith(prefix) || message.author.bot) return;
+  if(message.content.startsWith(prefix + "help"))
+  {
+    message.channel.send({embed: {
+        color: 3066993,
+        title: "Commands",
+        description: "Brinley The Motivational Bot supports the following commands. They are case-sensitive.",
+        fields: [{
+            name: "ps!help",
+            value: "Brings up this very message."
+          },
+          {
+            name: "ps!ping",
+            value: "Pings the bot to make sure that it is working."
+          },
+          {
+            name: "ps!motivate",
+            value: "Spreads motivation to the user that wrote the message."
+          },
+          {
+            name: "ps!version",
+            value: "Displays the current version of the bot."
+          }
+        ]
+      }
+    });
+  }
+  else if(message.content.startsWith(prefix + "ping"))
+  {
+    message.channel.send(`${message.author}, I am ready for your command!`);
+  }
+  else if(message.content.startsWith(prefix + "motivate"))
+  {
+    message.channel.send(`${message.author}, I hope you have a wonderful day!`);
+  }
+  else if(message.content.startsWith(prefix + "version"))
+  {
+    message.channel.send(`${message.author}, the current version is 1.0.0!`);
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
